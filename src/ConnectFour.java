3b5fc3a4e9d5f65fb7cb2821a581e9668e0b9e43
TuNisiAa-guy <federico.toluzzo1@gmail.com>
2023-12-02 10:43:17 +0100

    Started

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,17 +1,26 @@
 public class ConnectFour {
     private final int ROWS = 6;
     private final int COLUMNS = 7;
-    private Token[][] board;
+    private Token[][] board = new Token[ROWS][COLUMNS];
     public ConnectFour(){
-
+        fillBoard();
+        render();
     }
     private void render(){
         for (int i = 0; i < ROWS; i++) {
-            System.out.print("|");
+            System.out.print("| ");
             for (int j = 0; j < COLUMNS; j++) {
-                System.out.printf("%s|", board[i][j].getLabel());
+                System.out.printf("%s | ", board[i][j].getLabel());
             }
             System.out.println();
+        }
+        System.out.println("-----------------------------");
+    }
+    private void fillBoard(){
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                board[i][j] = new Token();
+            }
         }
     }
 }
